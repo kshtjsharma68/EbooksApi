f960ad80ba5080354d9ee09f4912bade7b65916f
kshitij sharma <kshtjsharma68@gmail.com>
2019-08-30 18:45:36 +0530

    crud start

--- controllers/crudController.js
+++ controllers/crudController.js
@@ -3,10 +3,43 @@ var app = express();
 var Blog = require('../models/blog');
 
 exports.create = (req, res, next) => {
-	console.log('inside controller')
+	let status = 400;
+	if (req.title && req.message) {
+		const blog  = new Blog();
+		blog.title  = req.title;
+		blog.text   = req.message; 
+		blog.save((err) => {
+			if(err) console.log(err)
+				console.log('data saved')
+		})
+	} 
+}
+
+exports.create = (req, res, next) => {
+	let {message, title} = req.body;
+	if( typeof message == 'undefined' || !message.length ) {
+		return res.json(400, {error: 'Message is invalid', message});
+	}
+	if (title && message) {
+		const blog  = new Blog();
+		blog.title  = req.title;
+		blog.text   = req.message;
+		blog.save((err) => {console.log('indes')
+			if(!err) console.log('data saved')	
+		})
+	}
+
+	let blogs = getAllBlogs();
+
+	res.status(200).json({blogs});
+	
+}
+
+const getAllBlogs = _ => {
+	return Blog.find({}).exec((err, blogs) => blogs);
 }
 
 exports.index = (req, res, next) => {
-	let blogs = Blog.find();
-	return res.render('blog', {status: 200, blogs});
+		let blogs = getAllBlogs();	
+	    res.render('blog', {status: 200, blogs}); 
 }
--- routes/random.js
+++ routes/random.js
@@ -7,14 +7,7 @@ router.get('/', (req, res, next) => {
 	res.sendFile(app.get('views')+'/chat.html');
 });
 
-router.post('/messgae', (req, res, next) => {
-	var msg = res.body.message;
-	if( _.isUndefined(msg) || _.isEmpty(msg.trim()) ) {
-		return res.json(400, {error: 'Message is invalid'});
-	}
-
-	res.json(200, {message: "Message recieved"});
-});
+router.post('/blog', crud.create);
 
 router.get('/blog', crud.index)
 
